3b08b3fad47c38e380f009662ce740cc73f30bb8
jackie007-bit <52102594+RaedKharrat@users.noreply.github.com>
2024-11-01 00:24:30 +0100

    mise a jour

--- src/component/Signin.jsx
+++ src/component/Signin.jsx
@@ -1,20 +1,30 @@
-import React, { useState } from 'react'; // Import useState for managing password visibility
-import './style.css'; // Ensure to create and import your CSS file
+import React, { useState } from 'react';
+import './style.css';
+import Modal from './Modal'; // Import the modal component
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faLock, faUser, faPhone, faMapMarkerAlt, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'; // Import necessary icons
-import logoapp from './logoAppDark.png'; // Import the logo image
-import logoapp2 from './logoAppDark2.png'; // Import the logo image
+import { faEnvelope, faLock, faUser, faPhone, faMapMarkerAlt, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import logoapp from './logoAppDark.png';
+import logoapp2 from './logoAppDark2.png';
 
 const LoginSignupForm = () => {
-  const [showPasswordLogin, setShowPasswordLogin] = useState(false); // State for login password visibility
-  const [showPasswordSignup, setShowPasswordSignup] = useState(false); // State for signup password visibility
+  const [showPasswordLogin, setShowPasswordLogin] = useState(false);
+  const [showPasswordSignup, setShowPasswordSignup] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false); // State for modal visibility
 
   const togglePasswordVisibilityLogin = () => {
-    setShowPasswordLogin(prev => !prev); // Toggle password visibility for login
+    setShowPasswordLogin(prev => !prev);
   };
 
   const togglePasswordVisibilitySignup = () => {
-    setShowPasswordSignup(prev => !prev); // Toggle password visibility for signup
+    setShowPasswordSignup(prev => !prev);
+  };
+
+  const handleForgotPasswordClick = () => {
+    setIsModalOpen(true); // Open the modal
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false); // Close the modal
   };
 
   return (
@@ -39,8 +49,8 @@ const LoginSignupForm = () => {
       <div className="forms">
         <div className="form-content">
           <div className="login-form">
-            <img src={logoapp2} alt="Logo" className="form-logo" style={{ marginBottom: '40px', height: '50px', width: 'auto' }} /> {/* Logo added here */}
-            <div className="title" style={{color:'#4E7A50'}}>Login</div>
+            <img src={logoapp2} alt="Logo" className="form-logo" style={{ marginBottom: '40px', height: '50px', width: 'auto' }} />
+            <div className="title" style={{ color: '#4E7A50' }}>Login</div>
             <form action="#">
               <div className="input-boxes">
                 <div className="input-box">
@@ -58,7 +68,7 @@ const LoginSignupForm = () => {
                     <FontAwesomeIcon icon={showPasswordLogin ? faEyeSlash : faEye} />
                   </span>
                 </div>
-                <div className="text"><a href="#">Forgot password?</a></div>
+                <div className="text"><a href="#" onClick={handleForgotPasswordClick}>Forgot password?</a></div> {/* Update the link to open the modal */}
                 <div className="button input-box">
                   <input type="submit" value="Submit" />
                 </div>
@@ -67,8 +77,8 @@ const LoginSignupForm = () => {
             </form>
           </div>
           <div className="signup-form">
-            <img src={logoapp} alt="Logo" className="form-logo" /> {/* Logo added here */}
-            <div className="title"style={{color:'#4E7A50'}}>Signup</div>
+            <img src={logoapp} alt="Logo" className="form-logo" />
+            <div className="title" style={{ color: '#4E7A50' }}>Signup</div>
             <form action="#">
               <div className="input-boxes">
                 <div className="input-box">
@@ -91,11 +101,11 @@ const LoginSignupForm = () => {
                   </span>
                 </div>
                 <div className="input-box">
-                  <FontAwesomeIcon icon={faPhone} /> {/* Phone icon for the phone number field */}
+                  <FontAwesomeIcon icon={faPhone} />
                   <input type="text" placeholder="+216 ** *** ***" required />
                 </div>
                 <div className="input-box">
-                  <FontAwesomeIcon icon={faMapMarkerAlt} /> {/* Map marker icon for the address field */}
+                  <FontAwesomeIcon icon={faMapMarkerAlt} />
                   <input type="text" placeholder="Enter your address" required />
                 </div>
                 <div className="button input-box">
@@ -107,6 +117,7 @@ const LoginSignupForm = () => {
           </div>
         </div>
       </div>
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal} /> {/* Include the modal */}
     </div>
   );
 };
